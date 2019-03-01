d73b5c45b1520632bc1d20aee8638615e5d44ae2
Emmanuel Omona <emmanuel.omona@andela.com>
2019-03-01 22:39:31 +0300

    chore(setupredux): setup react with redux
    - install redux
    - structure file organisation
    - setup actions
    - setup reducers
    - setup redux store
    
    [Finishes #164329093]

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from '../Navbar'
 import Home from '../Home';
+import Footer from '../Footer'
 
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
                     <div className="container">
                         <Route exact path='/' component={Home}/>
                     </div>
+                    <Footer />
                 </div>
             </BrowserRouter>
         )
